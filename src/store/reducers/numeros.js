43438e6d1096540bce39cabb72377ff9722a118a
Millena Araújo Martines <millenamartinez@pool-100-1-2-191.nwrknj.fios.verizon.net>
2024-06-18 13:49:58 -0300

    add pasta para organização

--- src/store/reducers/numeros.js
+++ src/store/reducers/numeros.js
@@ -1,3 +1,9 @@
+import {
+    NUM_MIN_ALTERADO,
+    NUM_MAX_ALTERADO
+} from'..actions/actionType'
+
+
 const initialSate = {
     min: 1,
     max: 10
@@ -6,12 +12,12 @@ const initialSate = {
 
 export default function (state = initialSate, action) {
     switch (action.type) {
-        case 'NUM_MIN_ALTERADO':
+        case NUM_MIN_ALTERADO:
             return {
                 ...state,
                 min: action.payload
             }
-        case 'NUM_MAX_ALTERADO':
+        case NUM_MAX_ALTERADO :
             return {
                 ...state,
                 max: action.payload
